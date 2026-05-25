ee1200f291f628536ab8ac1ce421d7d08c0ca8d2
Nathan Groover <nathan.groover@gmail.com>
2026-05-25 12:56:23 -0400

    Fix unit test.

--- environment/src/legal_actions.rs
+++ environment/src/legal_actions.rs
@@ -14,16 +14,17 @@ pub fn legal_actions(gs: &Gamestate) -> Vec<Action> {
 mod tests {
     use super::*;
     use crate::decks::{build_dorinthea_deck, build_rhinar_deck};
+    use crate::fab_game::{gamestate_from_decklists,reset};
 
     #[test]
     fn legal_actions_in_choose_first_phase() {
         let mut gs = gamestate_from_decklists(build_rhinar_deck(), build_dorinthea_deck());
-        reset(gs);
+        reset(&mut gs);
 
         let actions = legal_actions(&gs);
 
         assert_eq!(actions.len(), 2);
-        assert!(actions.contains(&Action::CHOOSE_FIRST));
-        assert!(actions.contains(&Action::CHOOSE_SECOND));
+        assert!(actions.contains(&Action::ChooseFirst));
+        assert!(actions.contains(&Action::ChooseSecond));
     }
 }
